759c7461a8845759b2e94942d301188a98b928e3
Daniel Douat <daniel.douat@aelys-info.fr>
2014-10-10 16:42:41 +0200

    added WIP recursive generation

--- generate.php
+++ generate.php
@@ -18,49 +18,75 @@ if (file_exists(SCRIPT_PATH . 'config.php')) {
     include SCRIPT_PATH . 'config.php';
 }
 
-if (is_dir(SCRIPT_PATH . $galleryPath) && file_exists(SCRIPT_PATH . $pageTemplate)) {
-    if (!is_dir(SCRIPT_PATH . $thumbsPath)) mkdir(SCRIPT_PATH . $thumbsPath);
-    $gallery = dir(SCRIPT_PATH . $galleryPath);
-    while (($entry = $gallery->read()) !== false) {
-        if (preg_match($imagePattern, $entry)) {
-            $imagesList[] = $entry;
-        }
+function generate($dirPath = '')
+{
+    $imageTag         = $GLOBALS['imageTag'];
+    $imageNoScriptTag = $GLOBALS['imageNoScriptTag'];
+    //$dirPath = rtrim($dirPath, '/');
+    if (empty($dirPath))  {
+        $dirPath = SCRIPT_PATH . $GLOBALS['galleryPath'];
+        $galleryBase = $GLOBALS['galleryBase'];
     }
-    if (is_array($imagesList)) {
-        ($sort === 'desc') ? rsort($imagesList) : sort($imagesList);
-        foreach ($imagesList as $key => $name) {
-            $imageUri                           = SCRIPT_PATH . $galleryPath . '/' . $name;
-            list($width, $height, $type, $attr) = getimagesize($imageUri);
-            $thumbHeight                        = round($height * $thumbWidth / $width);
-            if (!file_exists(SCRIPT_PATH . $thumbsPath . '/' . $name)) {
-                $source = imagecreatefromjpeg ($imageUri);
-                $thumb  = imagecreatetruecolor($thumbWidth, $thumbHeight);
-                imagecopyresampled($thumb, $source, 0, 0, 0, 0, $thumbWidth, $thumbHeight, $width, $height);
-                imagedestroy($source);
-                imagejpeg($thumb, SCRIPT_PATH . $thumbsPath . '/' . $name, 100);
+    else {
+        list($before, $after) = explode($GLOBALS['galleryPath'], $dirPath);
+        $galleryBase = $GLOBALS['galleryBase'] . $after;
+    }
+    if (is_dir($dirPath)) {
+        $thumbsDir   = $GLOBALS['thumbsDir'];
+        $thumbsPath  = $dirPath . '/' . $thumbsDir;
+        $galleryFile = $dirPath . '/' . $GLOBALS['galleryFile'];
+        if (!is_dir($thumbsPath)) mkdir($thumbsPath);
+        $gallery = dir($dirPath);
+        $noScan  = array('.', '..', $GLOBALS['thumbsDir']);
+        while (($entry = $gallery->read()) !== false) {
+            if (preg_match($GLOBALS['imagePattern'], $entry)) {
+                $imagesList[] = $entry;
             }
-            $currentImage         = str_replace('{thumbUri}', $galleryBase . '/' . $thumbsDir . '/' . $name, $imageTag);
-            $currentImage         = str_replace('{thumbWidth}', $thumbWidth, $currentImage);
-            $currentImage         = str_replace('{thumbHeight}', $thumbHeight, $currentImage);
-            $currentImage         = str_replace('{imageUri}', $galleryBase . '/' . $name, $currentImage);
-            $currentImage         = str_replace('{imageWidth}', $width, $currentImage);
-            $currentImage         = str_replace('{imageHeight}', $height, $currentImage);
-            $images[]             = $currentImage;
-            $currentImageNoScript = str_replace('{thumbUri}', $galleryBase . '/' . $thumbsDir . '/' . $name, $imageNoScriptTag);
-            $currentImageNoScript = str_replace('{thumbWidth}', $thumbWidth, $currentImageNoScript);
-            $currentImageNoScript = str_replace('{thumbHeight}', $thumbHeight, $currentImageNoScript);
-            $currentImageNoScript = str_replace('{imageUri}', $galleryBase . '/' . $name, $currentImageNoScript);
-            $currentImageNoScript = str_replace('{imageWidth}', $width, $currentImageNoScript);
-            $currentImageNoScript = str_replace('{imageHeight}', $height, $currentImageNoScript);
-            $imagesNoScript[]     = $currentImageNoScript;
+            elseif (!in_array($entry, $noScan) && is_dir($entry)) {
+                generate($dirPath);
+            }
         }
+        if (is_array($imagesList)) {
+            ($sort === 'desc') ? rsort($imagesList) : sort($imagesList);
+            foreach ($imagesList as $key => $name) {
+                $imageUri                           = $dirPath . '/' . $name;
+                list($width, $height, $type, $attr) = getimagesize($imageUri);
+                $thumbWidth                         = $GLOBALS['thumbWidth'];
+                $thumbHeight                        = round($height * $thumbWidth / $width);
+                if (!file_exists($thumbsPath . '/' . $name)) {
+                    $source = imagecreatefromjpeg ($imageUri);
+                    $thumb  = imagecreatetruecolor($thumbWidth, $thumbHeight);
+                    imagecopyresampled($thumb, $source, 0, 0, 0, 0, $thumbWidth, $thumbHeight, $width, $height);
+                    imagedestroy($source);
+                    imagejpeg($thumb, $thumbsPath . '/' . $name, 100);
+                }
+                $currentImage         = str_replace('{thumbUri}', $galleryBase . '/' . $thumbsDir . '/' . $name, $imageTag);
+                $currentImage         = str_replace('{thumbWidth}', $thumbWidth, $currentImage);
+                $currentImage         = str_replace('{thumbHeight}', $thumbHeight, $currentImage);
+                $currentImage         = str_replace('{imageUri}', $galleryBase . '/' . $name, $currentImage);
+                $currentImage         = str_replace('{imageWidth}', $width, $currentImage);
+                $currentImage         = str_replace('{imageHeight}', $height, $currentImage);
+                $images[]             = $currentImage;
+                $currentImageNoScript = str_replace('{thumbUri}', $galleryBase . '/' . $thumbsDir . '/' . $name, $imageNoScriptTag);
+                $currentImageNoScript = str_replace('{thumbWidth}', $thumbWidth, $currentImageNoScript);
+                $currentImageNoScript = str_replace('{thumbHeight}', $thumbHeight, $currentImageNoScript);
+                $currentImageNoScript = str_replace('{imageUri}', $galleryBase . '/' . $name, $currentImageNoScript);
+                $currentImageNoScript = str_replace('{imageWidth}', $width, $currentImageNoScript);
+                $currentImageNoScript = str_replace('{imageHeight}', $height, $currentImageNoScript);
+                $imagesNoScript[]     = $currentImageNoScript;
+            }
+        }
+        $page     = file_get_contents(SCRIPT_PATH . $GLOBALS['pageTemplate']);
+        $replace  = (is_array($images)) ? implode(PHP_EOL, $images) : '';
+        $noScript = (is_array($imagesNoScript)) ? implode(PHP_EOL, $imagesNoScript) : '';
+        $page     = str_replace('{galleryPath}', $galleryBase, $page);
+        $page     = str_replace('{images}', $replace, $page);
+        $page     = str_replace('{imagesNoScript}', $noScript, $page);
+        file_put_contents($galleryFile, $page, LOCK_EX);
     }
-    $page     = file_get_contents(SCRIPT_PATH . $pageTemplate);
-    $replace  = (is_array($images)) ? implode(PHP_EOL, $images) : '';
-    $noScript = (is_array($imagesNoScript)) ? implode(PHP_EOL, $imagesNoScript) : '';
-    $page     = str_replace('{galleryPath}', $galleryBase, $page);
-    $page     = str_replace('{images}', $replace, $page);
-    $page     = str_replace('{imagesNoScript}', $noScript, $page);
-    file_put_contents(SCRIPT_PATH . $galleryPath . '/' . $galleryFile, $page, LOCK_EX);
+}
+
+if (is_dir(SCRIPT_PATH . $galleryPath) && file_exists(SCRIPT_PATH . $pageTemplate)) {
+    generate();
 }
 ?>
